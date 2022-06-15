1b1fa7f315f2184efe98d2e3c31b1ba194904246
Xiphe <github@xiphe.net>
2022-06-15 23:00:42 +0200

    Revert "docs(via-url): install in via-url example"
    This reverts commit f52cfcfb7a4a321cfa4cce22be8d54dd78e60021.

--- examples/via-url/app/entry.server.tsx
+++ examples/via-url/app/entry.server.tsx
@@ -1,39 +1,15 @@
 import type { EntryContext } from '@remix-run/node';
 import { RemixServer } from '@remix-run/react';
 import { renderToString } from 'react-dom/server';
-import { setup as setupRemixPolyglot } from 'remix-polyglot';
-import { supportedLocales, getLocaleFromPathname } from '~/util/i18n';
-import localeManifest from './manifest-remix-polyglot.json';
 
-export default async function handleRequest(
+export default function handleRequest(
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
   remixContext: EntryContext,
 ) {
-  const { pathname } = new URL(request.url);
-  if (pathname === '/') {
-    return new Response(null, {
-      status: 302,
-      headers: {
-        location: `/${supportedLocales[0]}`,
-        'cache-control': 's-maxage=0,max-age=86400',
-      },
-    });
-  }
-
-  /* Note that you could also use other parts of the url to determine locale */
-  const locale = getLocaleFromPathname(pathname);
-  const RemixPolyglotProvider = await setupRemixPolyglot({
-    locale,
-    remixContext,
-    manifest: localeManifest,
-  });
-
   let markup = renderToString(
-    <RemixPolyglotProvider>
-      <RemixServer context={remixContext} url={request.url} />
-    </RemixPolyglotProvider>,
+    <RemixServer context={remixContext} url={request.url} />,
   );
 
   responseHeaders.set('Content-Type', 'text/html');
--- examples/via-url/app/root.tsx
+++ examples/via-url/app/root.tsx
@@ -3,15 +3,10 @@ import {
   Links,
   LiveReload,
   Meta,
+  Outlet,
   Scripts,
   ScrollRestoration,
-  Outlet,
-  useMatches,
-  useTransition,
 } from '@remix-run/react';
-import { useEffect, useState } from 'react';
-import { useLocale, Handoff as RemixPolyglotHandoff } from 'remix-polyglot';
-import { isSupportedLocale } from './util/i18n';
 
 export const meta: MetaFunction = () => ({
   charset: 'utf-8',
@@ -20,45 +15,18 @@ export const meta: MetaFunction = () => ({
 });
 
 export default function App() {
-  const locale = useLocale();
-  const navigating = useLoadingIndicator();
-
   return (
-    <html lang={locale} dir="ltr">
+    <html lang="en">
       <head>
         <Meta />
         <Links />
       </head>
       <body>
         <Outlet />
-        {navigating ? (
-          <>
-            <br />
-            👀 Navigating...
-          </>
-        ) : null}
         <ScrollRestoration />
-        <RemixPolyglotHandoff />
         <Scripts />
         <LiveReload />
       </body>
     </html>
   );
-}
-
-function useLoadingIndicator() {
-  const { state } = useTransition();
-  const [loading, setLoading] = useState<boolean>(false);
-  useEffect(() => {
-    if (state === 'idle') {
-      setLoading(false);
-      return;
-    }
-    const i = setTimeout(() => {
-      setLoading(true);
-    }, 300);
-    return () => clearTimeout(i);
-  }, [state]);
-
-  return loading;
 }
